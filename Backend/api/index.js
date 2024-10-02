d613e5778cc2234cd6ab11bcfa4a34e822d0351a
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-10-02 23:09:28 +0530

    updated index.js

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const paymentRoutes = require("./routes/paymentRoutes.js");
+const paymentRoutes = require("../routes/paymentRoutes.js");
 
 const app = express();
-app.use(express.json());
 
 app.use(
   cors({
