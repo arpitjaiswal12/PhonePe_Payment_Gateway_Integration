1c8d275e8a36a62036c0a3fcb37616ea67647446
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-10-02 22:18:45 +0530

    payment domain changes

--- Backend/utils/phonePeUtils.js
+++ Backend/utils/phonePeUtils.js
@@ -4,9 +4,9 @@ const MERCHANT_KEY = "48b460bd-1463-497b-a621-8f9f73e193cd";
 const MERCHANT_ID = "M22MU4WHSIF5F";
 const MERCHANT_BASE_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay";
 const MERCHANT_STATUS_URL = "https://api.phonepe.com/apis/hermes/pg/v1/status";
-const redirectUrl = "http://192.168.0.182:8000/status";
-const successUrl = "http://192.168.0.182:5173/payment-success";
-const failureUrl = "http://192.168.0.182:5173/payment-failure";
+const redirectUrl = "https://phone-pe-payment-gateway-integration.vercel.app/status";
+const successUrl = "https://phone-pe-payment-gateway-integration.vercel.app/payment-success";
+const failureUrl = "https://phone-pe-payment-gateway-integration.vercel.app/payment-failure";
 
 module.exports = {
     MERCHANT_KEY, MERCHANT_ID, MERCHANT_BASE_URL, MERCHANT_STATUS_URL,
