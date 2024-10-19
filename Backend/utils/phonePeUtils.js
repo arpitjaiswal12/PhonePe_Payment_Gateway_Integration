fd3e5695a161cbb387f5bd452175c52263b0bb1d
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-10-19 13:28:40 +0530

    production url added

--- Backend/utils/phonePeUtils.js
+++ Backend/utils/phonePeUtils.js
@@ -1,6 +1,6 @@
 const SALT_KEY = "48b460bd-1463-497b-a621-8f9f73e193cd";
 const MERCHANT_ID = "M22MU4WHSIF5F";
-const MERCHANT_BASE_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay";
+const MERCHANT_BASE_URL = "https://api.phonepe.com/apis/hermes";
 const MERCHANT_STATUS_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status";
 // const redirectUrl = "https://phone-pe-payment-gateway-integration.vercel.app/api/payment/status";
 // const failureUrl = "https://phone-pe-payment-gateway-integration.vercel.app/api/payment/payment-failure";
