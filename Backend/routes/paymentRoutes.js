3420faa1492213d8ee867aecb2c715eeefa6941a
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-10-24 18:16:14 +0530

    status url converted to post

--- Backend/routes/paymentRoutes.js
+++ Backend/routes/paymentRoutes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post('/create-order', createOrder);
 
 // Route for checking payment status
-router.get('/status/:id', paymentStatus);
+router.post('/status/:id', paymentStatus);
 
 module.exports = router;
